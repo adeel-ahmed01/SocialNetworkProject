3b6195850c6da9f6d00b973673e6f57bb77593e0
Adeel Ahmed <adeelh248@gmail.com>
2019-12-25 23:07:28 +0100

    Added partial security implementation

---
+++ src/main/java/com/example/demo/CustomUserDetails.java
@@ -0,0 +1,61 @@
+package com.example.demo;
+
+import com.example.demo.pojo.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+
+    private String email;
+    private String password;
+
+    public CustomUserDetails(String email) {
+        this.email = email;
+        // Can implement authorities
+    }
+
+    public CustomUserDetails(User byEmail) {
+        this.email = byEmail.getEmail();
+        this.password = byEmail.getPassword();
+        // Can implement authorities
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ADMIN"));
+    }
+
+    @Override
+    public String getPassword() {
+        return "123";
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,22 @@
 package com.example.demo;
 
+import com.example.demo.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 @Slf4j
-@SpringBootApplication(exclude = {RedisRepositoriesAutoConfiguration.class})
+@SpringBootApplication(exclude = {RedisRepositoriesAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -14,4 +24,7 @@ public class DemoApplication {
         log.info("DemoApplication is running ...");
     }
 
+    public void authenticationManager(AuthenticationManagerBuilder builder, UserRepository repository) throws Exception {
+        builder.userDetailsService(s -> new CustomUserDetails(repository.findByEmail(s)));
+    }
 }
